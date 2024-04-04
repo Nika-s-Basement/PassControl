f68a6b3cf76c1aa78207716ecb46f899f2c0abb6
lekasNet <lekas1988@gmail.com>
2024-04-04 21:51:42 +0300

    trying to do page fancy

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainPage(),
+      home: MyHomePage(),
     );
   }
 }
 
-class MainPage extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
   @override
-  _MainPageState createState() => _MainPageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MainPageState extends State<MainPage> {
-  bool _isExpanded = false;
+class _MyHomePageState extends State<MyHomePage> {
+  bool isExpanded = false;
+  final double logoWidth = 150.0;
+  final double menuWidth = (4 + 11 + 19 + 23 + 10 + 10 + 10) * 5; // Примерно рассчитываем ширину меню
 
   @override
   Widget build(BuildContext context) {
+    double totalWidth = MediaQuery.of(context).size.width;
+    var titles = ['Лоты', 'Новости', 'Контакты', 'Вход'];
+    var sum = 0;
+    var lenghts = [];
+    for (String element in titles) {sum += element.length; lenghts.add(sum + 10);}
+    print(lenghts);
+    lenghts = [0, 4, 7, 8];
+
     return Scaffold(
       appBar: AppBar(
-        title: InkWell(
-          onTap: () {
-            setState(() {
-              _isExpanded = !_isExpanded;
-            });
-          },
+        title: SizedBox(
+          width: totalWidth,
+          height: 56,
           child: Stack(
-            alignment: Alignment.center,
-            children: <Widget>[
-              // Логотип
-              Container(
-                child: Image.asset('web/icons/icon-192.png', width: 50)
-              ),
-              // Кнопки, которые появляются
+            children: [
+              for (int i = 0; i < 4; i++)
+                AnimatedPositioned(
+                  duration: Duration(milliseconds: 500),
+                  curve: Curves.easeInOut,
+                  left: isExpanded ? totalWidth / 2 - menuWidth / 2 + lenghts[i] * 30 : totalWidth / 2 - logoWidth / 2,
+                  child: _buildMenuButton(titles[i]),
+                ),
               AnimatedPositioned(
                 duration: Duration(milliseconds: 500),
-                left: _isExpanded ? 0 : MediaQuery.of(context).size.width / 2 - 50, // Начальное и конечное положение кнопок
-                right: _isExpanded ? 0 : MediaQuery.of(context).size.width / 2 - 50,
-                top: _isExpanded ? 60 : 0, // Высота появления кнопок
-                child: Opacity(
-                  opacity: _isExpanded ? 1 : 0,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      _buildButton('Лоты'),
-                      _buildButton('Новости'),
-                      _buildButton('Контакты'),
-                      _buildButton('Вход'),
-                    ],
+                curve: Curves.easeInOut,
+                left: isExpanded ? totalWidth / 2 - menuWidth / 2 - logoWidth : totalWidth / 2 - logoWidth / 2 - 50,
+                child: InkWell(
+                  onTap: () {
+                    setState(() {
+                      isExpanded = !isExpanded;
+                    });
+                  },
+                  child: Container(
+                    width: logoWidth,
+                    height: 56,
+                    color: Colors.white,
+                    child: Image.asset('web/icons/icon-192.png', width: 50,), // Замените на ваш логотип
+                    alignment: Alignment.centerRight,
                   ),
                 ),
               ),
             ],
           ),
         ),
-        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
       ),
       body: Center(
-        child: Text('Главный экран'),
+        child: Text('Главная страница'),
       ),
     );
   }
 
-  Widget _buildButton(String title) {
-    return ElevatedButton(
+  Widget _buildMenuButton(String title) {
+    return TextButton(
       onPressed: () {
-        // Ваша логика навигации здесь
+        // Добавьте сюда вашу логику перехода
         print('$title pressed');
       },
-      child: Text(title),
+      child: Text(
+        title,
+        style: GoogleFonts.montserrat(
+          fontWeight: FontWeight.bold,
+          fontSize: 18,
+          color: Colors.black,
+        ),
+      ),
+      style: TextButton.styleFrom(
+        backgroundColor: Colors.transparent,
+        padding: EdgeInsets.zero,
+      ),
     );
   }
 }
-
